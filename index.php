cd6d1e70e332689351c07283b5c656fcac92a256
MisterrVictor <85834329+MisterrVictor@users.noreply.github.com>
2023-05-05 15:55:54 +0300

    Update index.php
    Task 1 + task 2

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
 
 require_once 'Person.php';
+require_once 'PeopleList.php';
+require_once 'Database.php';
 
+// Пример использования task1
 $person = new Person(null, 'John', 'Doe', '1990-05-15', 0, 'New York');
 $person->save();
 var_dump($person);
@@ -15,4 +18,18 @@ echo "Formated Birthdate: " . $person_formated->birthdate . "<br>";
 echo "Formated Gender: " . $person_formated->gender . "<br>";
 echo "Birth City: " . $person_formated->birth_city . "<br>";
 
+// Пример использования task2
+$ids = array();
+$conn = (new Database())->getConnection();
+$stmt = $conn->prepare("SELECT `id` FROM `people`");
+$stmt->execute();
+$row = $stmt->fetch();
 
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    $ids[] = $row['id'];
+}
+var_dump($ids);
+$people_list = new PeopleList($ids);
+$people = $people_list->getPeople();
+var_dump($people);
+$people_list->deletePeople();
